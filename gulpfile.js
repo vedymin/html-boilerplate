19d464334db9621f004160641c5223ef828f34bc
Dawid Michalski <michalskimlm@gmail.com>
2020-02-11 19:31:48 +0100

    Add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-//compile
-gulp.task("sass", async function() {
-  gulp
-    .src("app/scss/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("app/css"));
-});
+// compile scss into css
+function style() {
+  // 1. source
+  return gulp.src('./scss/main.scss')
+    // 2. compile
+    .pipe(sass().on('error', sass.logError))
+    // 3. dest
+    .pipe(gulp.dest('./app/css'))
+}
+
+function watch() {
+  gulp.watch('./scss/**/*.scss', style);
+}
+
+exports.style = style;
+exports.watch = watch;
